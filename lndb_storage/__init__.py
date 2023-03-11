70d729c1d90c8830f7c7e5b92c3d7eed8150d33e
Koncopd <koncopd@gmail.com>
2023-03-11 18:16:41 +0100

    move upath here

--- lndb_storage/__init__.py
+++ lndb_storage/__init__.py
@@ -21,4 +21,5 @@ from ._file import delete_storage, load_to_memory, store_object
 from ._filesystem import _infer_filesystem
 from ._h5ad import h5ad_to_anndata
 from ._images import store_png
+from ._upath_ext import UPath
 from ._zarr import read_adata_zarr, write_adata_zarr
