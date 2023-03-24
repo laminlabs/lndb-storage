cf5d80b837fc7b75dc3fbf68d539f89588bfa2e3
Alex Wolf <f.alexander.wolf@gmail.com>
2023-03-25 00:01:31 +0100

    🚚 Rename `DObject` to `File` (#8)
    * :truck: Rename DObject to File
    
    * :construction_worker: Install lamindb from staging

--- lndb_storage/_subset.py
+++ lndb_storage/_subset.py
@@ -2,24 +2,24 @@ from typing import List, Optional, Union
 
 from anndata import AnnData, concat
 from lamin_logger import logger
-from lnschema_core import DObject
+from lnschema_core import File
 
-from .object import LazySelector, _subset_anndata_dobject
+from .object import LazySelector, _subset_anndata_file
 
 SUFFIXES = (".h5ad", ".zarr")
 
 
 def subset(
-    dobjects: Union[List[DObject], DObject],
+    files: Union[List[File], File],
     query_obs: Optional[Union[List[str], str, LazySelector]] = None,
     query_var: Optional[Union[List[str], str, LazySelector]] = None,
     use_concat: bool = False,
     concat_args: Optional[dict] = None,
 ) -> Union[List[AnnData], AnnData, None]:
-    """Subset AnnData dobjects and stream results into memory.
+    """Subset AnnData files and stream results into memory.
 
     Args:
-        dobjects: A `DObject` or a list of `DObjects` containing `AnnData` objects
+        files: A `File` or a list of `Files` containing `AnnData` objects
         to subset and load into memory.
         query_obs: The pandas query string to evaluate on `.obs` of each
         underlying `AnnData` object.
@@ -29,28 +29,28 @@ def subset(
         the returned `AnnData` objects.
         concat_args: Arguments for concatenation.
     """
-    if isinstance(dobjects, DObject):
-        dobjects = [dobjects]
+    if isinstance(files, File):
+        files = [files]
 
-    n_dobjects = len(dobjects)
+    n_files = len(files)
     if isinstance(query_obs, list):
-        if len(query_obs) != n_dobjects:
-            raise ValueError("query_obs list should be the same length as dobjects.")
+        if len(query_obs) != n_files:
+            raise ValueError("query_obs list should be the same length as files.")
     else:
-        query_obs = [query_obs] * n_dobjects  # type: ignore
+        query_obs = [query_obs] * n_files  # type: ignore
     if isinstance(query_var, list):
-        if len(query_var) != n_dobjects:
-            raise ValueError("query_var list should be the same length as dobjects.")
+        if len(query_var) != n_files:
+            raise ValueError("query_var list should be the same length as files.")
     else:
-        query_var = [query_var] * n_dobjects  # type: ignore
+        query_var = [query_var] * n_files  # type: ignore
 
     adatas = []
     # todo: implement parallel processing
-    for i, dobject in enumerate(dobjects):
-        if dobject.suffix not in SUFFIXES:
-            logger.warning(f"DObject {dobject.id} is not an AnnData object, ignoring.")
+    for i, file in enumerate(files):
+        if file.suffix not in SUFFIXES:
+            logger.warning(f"File {file.id} is not an AnnData object, ignoring.")
             continue
-        result = _subset_anndata_dobject(dobject, query_obs[i], query_var[i])
+        result = _subset_anndata_file(file, query_obs[i], query_var[i])
         if result is not None:
             adatas.append(result)
 
--- noxfile.py
+++ noxfile.py
@@ -1,4 +1,5 @@
 import nox
+import requests  # type: ignore
 from laminci import move_built_docs_to_docs_slash_project_slug, upload_docs_dir
 from laminci.nox import build_docs, login_testuser1, run_pre_commit, run_pytest
 
@@ -14,6 +15,11 @@ def lint(session: nox.Session) -> None:
 def build(session):
     login_testuser1(session)
     session.install(".[dev,test]")
+    response = requests.get("https://github.com/laminlabs/lamindb/tree/staging")
+    if response.status_code < 400:
+        session.install("git+https://github.com/laminlabs/lamindb@staging")
+    else:
+        session.install("git+https://github.com/laminlabs/lamindb")
     run_pytest(session)
     build_docs(session)
     upload_docs_dir()
