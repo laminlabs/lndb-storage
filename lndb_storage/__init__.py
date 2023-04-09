e81bc61d1a4606b14d06e1e605edb0964859f63c
Alex Wolf <f.alexander.wolf@gmail.com>
2023-04-09 19:22:01 +0200

    :green_heart: Attempt fix CI

--- lndb_storage/__init__.py
+++ lndb_storage/__init__.py
@@ -19,10 +19,11 @@ Store files:
 
 __version__ = "0.2rc2"  # denote a release candidate for 0.1.0 with 0.1rc1
 
+from lndb.dev.upath import UPath
+from lndb.dev.upath import infer_filesystem as _infer_filesystem
+
 from ._file import delete_storage, load_to_memory, store_object
-from ._filesystem import _infer_filesystem
 from ._h5ad import h5ad_to_anndata
 from ._images import store_png
 from ._subset import subset
-from ._upath_ext import UPath
 from ._zarr import read_adata_zarr, write_adata_zarr
--- lndb_storage/_filesystem.py
+++
@@ -1,1 +0,0 @@
-from lndb.dev.upath import infer_filesystem as _infer_filesystem  # noqa
